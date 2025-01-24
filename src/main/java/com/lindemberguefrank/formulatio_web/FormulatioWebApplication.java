568a180a6a041d1c99c396320c66867dfd861a61
LindembergueFrank <lindemberg.frank@gmail.com>
2025-01-24 14:33:53 -0300

    feat: starting project

---
+++ src/main/java/com/lindemberguefrank/formulatio_web/FormulatioWebApplication.java
@@ -0,0 +1,13 @@
+package com.lindemberguefrank.formulatio_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulatioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulatioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lindemberguefrank/formulatio_web/FormulatioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindemberguefrank.formulatio_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulatioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
