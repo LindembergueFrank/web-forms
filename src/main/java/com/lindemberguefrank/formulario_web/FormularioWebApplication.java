7b7c66e2370860614c29dcdaf62b9f97efd53703
LindembergueFrank <lindemberg.frank@gmail.com>
2025-01-26 10:20:14 -0300

    chore: rename directories

--- src/main/java/com/lindemberguefrank/formulario_web/FormularioWebApplication.java
+++ src/main/java/com/lindemberguefrank/formulario_web/FormularioWebApplication.java
@@ -1,13 +1,13 @@
-package com.lindemberguefrank.formulatio_web;
+package com.lindemberguefrank.formulario_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FormulatioWebApplication {
+public class FormularioWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FormulatioWebApplication.class, args);
+		SpringApplication.run(FormularioWebApplication.class, args);
 	}
 
 }
